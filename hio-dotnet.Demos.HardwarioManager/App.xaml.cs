9fa4bcc32322f987354e34ab982cafe3736f1018
Tomas Svoboda <tomas.svoboda@hardwario.com>
2024-10-11 11:23:56 -0400

    Added basic MAUI app for future demo of hio manager

---
+++ hio-dotnet.Demos.HardwarioManager/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace hio_dotnet.Demos.HardwarioManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ hio-dotnet.Demos.HardwarioManager/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace hio_dotnet.Demos.HardwarioManager
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
