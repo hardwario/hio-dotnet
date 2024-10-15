2f2346bea97421812b5592da5c55a36d5ecc3826
Tomas Svoboda <tomas.svoboda@hardwario.com>
2024-10-15 15:10:35 -0400

    Add project for BlazorComponents library and demo

---
+++ hio-dotnet.UI.BlazorComponents/Basics/BasicEnums.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hio_dotnet.UI.BlazorComponents.Basics
+{
+    public enum RowWidth 
+    { 
+        Full = 100, 
+        Half = 50, 
+        Quarter = 25, 
+        Auto, 
+        Custom 
+    }
+
+    public enum ColumnSize 
+    { 
+        Sm, 
+        Md, 
+        Lg, 
+        Xl, 
+        Xxl 
+    }
+
+    public enum ColumnWidth 
+    { 
+        Auto = 0,
+        One = 1, 
+        Two, 
+        Three, 
+        Four, 
+        Five, 
+        Six, 
+        Seven, 
+        Eight, 
+        Nine, 
+        Ten, 
+        Eleven, 
+        Twelve 
+    }
+
+    public enum ColumnWidthPercentage 
+    { 
+        Full = 100, 
+        Half = 50, 
+        Quarter = 25, 
+        Auto, 
+        Custom 
+    }
+
+}
