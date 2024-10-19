884854ce4513631caa6130f67d8622a9dab0ac35
Tomas Svoboda <tomas.svoboda@hardwario.com>
2024-10-19 09:06:29 -0500

    Components: Added Table and Container

--- hio-dotnet.UI.BlazorComponents/Basics/BasicEnums.cs
+++ hio-dotnet.UI.BlazorComponents/Basics/BasicEnums.cs
@@ -28,52 +28,59 @@ namespace hio_dotnet.UI.BlazorComponents.Basics
         Large
     }
 
+    public enum ContainerType
+    {
+        Default,    // Corresponds to Bootstrap's .container class
+        Fluid,      // Corresponds to Bootstrap's .container-fluid class
+        Custom      // Allows for a custom container class
+    }
+
 
     #region RowSpecific
-    public enum RowWidth 
-    { 
-        Full = 100, 
-        Half = 50, 
-        Quarter = 25, 
-        Auto, 
-        Custom 
+    public enum RowWidth
+    {
+        Full = 100,
+        Half = 50,
+        Quarter = 25,
+        Auto,
+        Custom
     }
     #endregion
 
     #region ColumnSpecific
-    public enum ColumnSize 
-    { 
-        Sm, 
-        Md, 
-        Lg, 
-        Xl, 
-        Xxl 
+    public enum ColumnSize
+    {
+        Sm,
+        Md,
+        Lg,
+        Xl,
+        Xxl
     }
 
-    public enum ColumnWidth 
-    { 
+    public enum ColumnWidth
+    {
         Auto = 0,
-        One = 1, 
-        Two, 
-        Three, 
-        Four, 
-        Five, 
-        Six, 
-        Seven, 
-        Eight, 
-        Nine, 
-        Ten, 
-        Eleven, 
-        Twelve 
+        One = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Eleven,
+        Twelve
     }
 
-    public enum ColumnWidthPercentage 
-    { 
-        Full = 100, 
-        Half = 50, 
-        Quarter = 25, 
-        Auto, 
-        Custom 
+    public enum ColumnWidthPercentage
+    {
+        Full = 100,
+        Half = 50,
+        Quarter = 25,
+        Auto,
+        Custom
     }
     #endregion
 
@@ -88,6 +95,7 @@ namespace hio_dotnet.UI.BlazorComponents.Basics
 
     #endregion
 
+    #region TextSpecific
     public enum TextEditWidthPercentage
     {
         Full = 100,
@@ -96,4 +104,28 @@ namespace hio_dotnet.UI.BlazorComponents.Basics
         Auto,
         Custom
     }
+
+    #endregion
+
+    #region TablesSpecific
+    public enum TableWidth
+    {
+        Auto,
+        Full,
+        Half,
+        Quarter,
+        Custom
+    }
+
+    public enum TableVariant
+    {
+        Default,
+        Striped,
+        Bordered,
+        Hover,
+        Condensed,
+        Custom
+    }
+
+    #endregion
 }
