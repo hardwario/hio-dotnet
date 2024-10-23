09c08d27e0fc1dc8284f912473dd4085e2f28b28
Tomas Svoboda <tomas.svoboda@hardwario.com>
2024-10-23 09:42:25 -0500

    Components: added basic console and search chester component

--- hio-dotnet.Demos.BlazorComponents.Radzen.WASM/Program.cs
+++ hio-dotnet.Demos.BlazorComponents.Radzen.WASM/Program.cs
@@ -1,11 +1,13 @@
 using hio_dotnet.Demos.BlazorComponents.Radzen.WASM;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddRadzenComponents();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
