3af533945bc16c165616594f5d03c077208458ae
Tomas Svoboda <tomas.svoboda@hardwario.com>
2025-02-05 18:42:34 -0400

    Common: make nullabe and ignore null json

--- hio-dotnet.Common/Models/Common/Measurement.cs
+++ hio-dotnet.Common/Models/Common/Measurement.cs
@@ -12,18 +12,22 @@ namespace hio_dotnet.Common.Models.Common
     {
         [SimulationAttribute(false)]
         [JsonPropertyName("min")]
-        public double Min { get; set; } = 0.0;
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public double? Min { get; set; } = 0.0;
 
         [SimulationAttribute(false)]
         [JsonPropertyName("max")]
-        public double Max { get; set; } = 0.0;
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public double? Max { get; set; } = 0.0;
 
         [SimulationAttribute(false)]
         [JsonPropertyName("avg")]
-        public double Avg { get; set; } = 0.0;
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public double? Avg { get; set; } = 0.0;
 
         [SimulationAttribute(false)]
         [JsonPropertyName("mdn")]
-        public double Mdn { get; set; } = 0.0;
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public double? Mdn { get; set; } = 0.0;
     }
 }
